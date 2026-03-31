5c8733d7d8dc5be53fa4d965783ea4b0ba3393ca
ᯓ‌𐌌།⃪᱂ ⚠️𝑻𝒉𝒂𝒌𝒖𝒓𓂃 <reetsingh274@gmail.com>
2026-04-01 02:22:24 +0530

    Update ping.py

--- Banword/modules/ping.py
+++ Banword/modules/ping.py
@@ -35,7 +35,7 @@ async def ping_command(_, message: Message):
 
     buttons = [
         [
-            InlineKeyboardButton("• ᴜᴘᴅᴀᴛᴇ •", url="https://t.me/networkxlog")
+            InlineKeyboardButton("• ᴜᴘᴅᴀᴛᴇ •", url="https://t.me/BOTxBOOSTER")
         ]
     ]
 
